cc37e992a6724ec45956a56bc246f1c4aa898313
Liam Le <154930237+liamlecs@users.noreply.github.com>
2024-03-15 20:35:27 -0500

    Update Footer.jsx
    Change href for Resume

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
     <div className="Footer--Container">
       <a href="https://github.com/liamlecs"><img class="Footer--Icon Footer--Icon--Github" src={GitHub}/>
       </a>
-      <a href="https://drive.google.com/file/d/1aJmXYxVqC0eIpvxgmdzgG-Z3vqlv0p82/view?usp=sharing"><img class="Footer--Icon Footer--Icon--Resume" src={Resume}/></a>
+      <a href="https://drive.google.com/file/d/1H6C-8zJ4VUWggUCFuNJXHV1T8tsmQ2Yy/view?usp=sharing"><img class="Footer--Icon Footer--Icon--Resume" src={Resume}/></a>
     </div>
     </>
   )
